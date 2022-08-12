e282e6178a8577e8552712271b27da4282857d26
arunavan <arunajava567@gmail.com>
2022-08-12 11:34:01 +0530

    Add files via upload

---
+++ JunitModule/JunitApp1/src/Loan.java
@@ -0,0 +1,15 @@
+
+public class Loan {
+	
+	public int getEmi(int amount) {
+		return amount/12;
+	}
+	
+
+	/*
+	 * public static void main(String[] args) { Loan l=new Loan();
+	 * System.out.println(l.getEmi(60000));
+	 * 
+	 * }
+	 */
+}
