bf9d6e88da79e42da64f4130e2d41a6e5573a0dc
arunavan <arunajava567@gmail.com>
2022-08-24 11:47:44 +0530

    Add files via upload

---
+++ Actuator/Actuator1/src/main/java/com/cg/Actuator1/SimpleRestController.java
@@ -0,0 +1,14 @@
+package com.cg.Actuator1;
+
+import java.util.Date;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class SimpleRestController {
+    @GetMapping("/example")
+    public String example() {
+        return "Hello User !! " + new Date();
+    }
+}
